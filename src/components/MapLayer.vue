f6d8f80c1ec66d43457e0ad7df0135c8f081cafd
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-07-01 12:41:25 +0200

    cleaning up readme

--- src/components/MapLayer.vue
+++ src/components/MapLayer.vue
@@ -1,4 +1,30 @@
 <template>
+  <section class="hero">
+    <div class="hero-body">
+      <p class="title">
+        Swimming photogrammetry lawnmowner trajectory capture
+      </p>
+      <p class="subtitle">
+        Capturing a swimming trajectory in shallow water and exporting for Emlid Reach RS.
+      </p>
+      <p>
+        Tools exist to draw trajectories for drones in order to upload trajectories and send them to collect aerial
+        images for photogrammetry.
+        However, these tools are not suited to compute such trajectory at low altitudes, typically when surveying
+        shallow water (it's mainly a matter of parameter ranges).
+      </p>
+      <div>
+        We propose here a tool to:
+        <ol>
+          <li>capture the surveying area</li>
+          <li>Selecting a camera, the view mode, and shot time intervals;</li>
+          <li>The distance to the ground, and picture overlaps;</li>
+          <li>The distance between the reported point on the map (swimming to one to the next), the angle of the route (take two in order to limit bowing effects), and the point naming;</li>
+        </ol>
+        You will then be able to export the route and import it in an Emlid ReachView project.
+      </div>
+    </div>
+  </section>
   <div>
     <div id="mapViewport" class="mapViewport map-viewport">
     </div>
@@ -19,6 +45,15 @@
     </div>
     <div id="mouse-position"></div>
   </div>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        By <a href="https://www.linkedin.com/in/alexmass/">Alexandre Masselot</a>, with Terra Submersa at University of Geneva.
+        The source code is available on <a href="https://github.com/terra-submersa/coordinates-lawnmower">Github</a> and licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts">
